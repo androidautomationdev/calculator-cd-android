9d36ee09ec299010db8c82d6cf94d72a6ba11317
Pavel Jacko <beherithrone@gmail.com>
2015-10-12 14:19:10 +0100

    hockeyapp upload

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 PRODUCT_NAME=hockey
 NOTIFY="True"
 NOTES="Build uploaded via the upload API"
@@ -7,19 +8,20 @@ echo "Archives: ${CIRCLE_ARTIFACTS}"
 
 if [ "$1" ]
 then
-  NOTES="$1"
+   NOTES="$1"
 fi
 
 if [ ! -f "app/build/outputs/apk/app-debug.apk" ]
 then
-  echo "app/build/outputs/apk/app-debug.apk not found!"
+   echo "app/build/outputs/apk/app-debug.apk not found!"
 else
-  echo "Uploading to HockeyApp..."
-  /usr/bin/curl "https://rink.hockeyapp.net/api/2/apps/upload" \
-       -F ipa=@"app/build/outputs/apk/app-debug.apk" \
-      -F "status=2" \
-       -F "notify=1" \
-       -F "notes=Some new features and fixed bugs." \
-       -F "notes_type=0" \
-       -H "X-HockeyAppToken: ${HOCKEYAPP_TOKEN}" \
+   echo "Uploading to HockeyApp..."
+   /usr/bin/curl "https://rink.hockeyapp.net/api/2/apps/upload" \
+        -F ipa=@"app/build/outputs/apk/app-debug.apk" \
+	    -F "status=2" \
+        -F "notify=1" \
+        -F "notes=Some new features and fixed bugs." \
+        -F "notes_type=0" \
+        -H "X-HockeyAppToken: ${HOCKEYAPP_TOKEN}" \
+#   /usr/bin/open "https://testflightapp.com/dashboard/builds/"
 fi
